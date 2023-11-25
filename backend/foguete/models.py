c78691266a4c7f899e4ed490cb28b2fa5cf6e25f
JoaoBarreto03 <211039519@aluno.unb.br>
2023-11-25 13:42:35 -0300

    Atualizando models

--- backend/foguete/models.py
+++ backend/foguete/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Voo(models.Model):
-    idVoo = models.IntegerField().PrimaryKey()
+    idVoo = models.IntegerField(primary_key=True)
     volumeinicialAgua = models.DecimalField(max_digits=6, decimal_places=2)
     pressaoInicial = models.DecimalField(max_digits=6, decimal_places=2)
     pesoFoguete = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,20 +13,20 @@ class Voo(models.Model):
 
 
 class Instante(models.Model):
-    idInstante = models.IntegerField().PrimaryKey()
+    idInstante = models.IntegerField(primary_key=True)
     tempo = models.TimeField()
     latitude = models.DecimalField(max_digits=10, decimal_places=6)
     longitude = models.DecimalField(max_digits=10, decimal_places=6)
     altitude = models.DecimalField(max_digits=6, decimal_places=2)
-    idVoo = models.IntegerField().ForeignKey(Voo, on_delete=models.CASCADE)
+    idVoo = models.ForeignKey(Voo, on_delete=models.CASCADE)
 
 class Eixo(models.Model):
-    idEixo = models.IntegerField().PrimaryKey()
+    idEixo = models.IntegerField(primary_key=True)
     angulo = models.DecimalField(max_digits=4, decimal_places=2)
     velocidade = models.DecimalField(max_digits=4, decimal_places=2)
     aceleracao = models.DecimalField(max_digits=4, decimal_places=2)
     tipoEixo = models.CharField(max_length=1)
-    idInstante = models.IntegerField().ForeignKey(Instante, on_delete=models.CASCADE)
+    idInstante = models.ForeignKey(Instante, on_delete=models.CASCADE)
 
     
 
