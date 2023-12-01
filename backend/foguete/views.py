ae2d9f5d51375fcca342bce10c7238ad785a35dc
Caio Santos <caiobs10@gmail.com>
2023-12-01 14:17:48 -0300

    Update views.py
    cria view nova apenas para enviar a quantidade de voos para o front

--- backend/foguete/views.py
+++ backend/foguete/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework import viewsets
-
+from rest_framework.response import Response
 from .models import Voo, Instante
-from .serializers import VooSerializer, InstanteSerializer
+from .serializers import VooSerializer, InstanteSerializer, CountElementsSerializer
 
+
+class CountElementsView(APIView):
+    def get(self, request, format=None):
+        # Get the count of elements in the Voo model
+        voo_count = Voo.objects.count()
+
+        # Serialize the response
+        serializer = CountElementsSerializer({'voo_count': voo_count})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class VooViewSet(viewsets.ModelViewSet):
     serializer_class = VooSerializer
